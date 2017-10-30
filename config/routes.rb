b734c5af360290bea66061401e336d5a2fec69b3
Thibaud Marcé <thibaud.marce@gmail.com>
2017-10-30 13:39:09 -0400

    implemented a grid generator taking a number as input

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'game', to: 'countdown#game'
+
+  get 'score', to: 'countdown#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
